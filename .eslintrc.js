89d182b69dc176e9f576fb09c357d835e8a5bb44
Elias Feijó <eliasfeijo.dev@gmail.com>
2021-08-31 01:12:26 -0300

    eslint new config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,38 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 module.exports = {
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react", "@typescript-eslint", "import"],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: "module",
+    project: "./tsconfig.json",
   },
-  plugins: [
-    "@typescript-eslint",
-    "react-hooks",
-  ],
-  extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
   rules: {
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "react/prop-types": "off",
-  },
-  settings: {
-    react: {
-      pragma: "React",
-      version: "detect",
-    },
+    "linebreak-style": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
   },
 };
