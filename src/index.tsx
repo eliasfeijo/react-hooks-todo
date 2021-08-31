e852d61d07c78c682335fb3e6285f6b2b9aa4d0f
Elias Feijó <eliasfeijo.dev@gmail.com>
2021-08-31 01:12:39 -0300

    eslint fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "tailwindcss/tailwind.css"
+import "tailwindcss/tailwind.css";
 
-const App = () => (
-  <h1>My React and TypeScript App!</h1>
-);
+const App = () => <h1>My React and TypeScript App!</h1>;
 
 ReactDOM.render(
   <React.StrictMode>
