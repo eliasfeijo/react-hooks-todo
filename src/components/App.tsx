10ce72b7eb4eab3b1f8e3ba79bddc7fa792b3ab4
Elias Feijó <eliasfeijo.dev@gmail.com>
2021-08-31 07:19:51 -0300

    Footer component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,9 +3,10 @@ import { TodosProvider } from "../contexts/TodosContext";
 import CreateTodo from "./Todo/CreateTodo";
 import ListTodo from "./Todo/ListTodo";
 import "./App.css";
+import Footer from "./Footer";
 
 const App = (): JSX.Element => (
-  <div className="h-full">
+  <div className="h-full flex flex-col">
     <h1 className="pt-4 px-4 text-4xl text-center">Todo Application</h1>
     <div className="p-4">
       <TodosProvider>
@@ -13,6 +14,8 @@ const App = (): JSX.Element => (
         <ListTodo />
       </TodosProvider>
     </div>
+    <div className="flex-1" />
+    <Footer />
   </div>
 );
 
