dd5cb5f82d15eeaa1459b4a58a566a28946d65c3
Elias Feijó <eliasfeijo.dev@gmail.com>
2021-08-31 07:12:41 -0300

    body height 100%

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import { TodosProvider } from "../contexts/TodosContext";
 import CreateTodo from "./Todo/CreateTodo";
 import ListTodo from "./Todo/ListTodo";
+import "./App.css";
 
 const App = (): JSX.Element => (
-  <div className="p-4">
-    <h1 className="text-4xl text-center">Todo Application</h1>
-    <TodosProvider>
-      <CreateTodo />
-      <ListTodo />
-    </TodosProvider>
+  <div className="h-full">
+    <h1 className="pt-4 px-4 text-4xl text-center">Todo Application</h1>
+    <div className="p-4">
+      <TodosProvider>
+        <CreateTodo />
+        <ListTodo />
+      </TodosProvider>
+    </div>
   </div>
 );
 
