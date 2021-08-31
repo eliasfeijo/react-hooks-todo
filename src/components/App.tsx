7687f36f0423578a930a0db766b2af07089a3f01
Elias Feijó <eliasfeijo.dev@gmail.com>
2021-08-31 05:20:30 -0300

    using TodosProvider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { TodosProvider } from "../contexts/TodosContext";
 import CreateTodo from "./Todo/CreateTodo";
 import ListTodo from "./Todo/ListTodo";
 
 const App = (): JSX.Element => (
   <div className="p-4">
     <h1 className="text-4xl text-center">Todo Application</h1>
-    <CreateTodo />
-    <ListTodo />
+    <TodosProvider>
+      <CreateTodo />
+      <ListTodo />
+    </TodosProvider>
   </div>
 );
 
